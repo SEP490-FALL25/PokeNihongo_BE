36853a472ed91e50c955dd13d5cfa1c188fcbc34
Kumo <145751369+thuanhd2102@users.noreply.github.com>
2025-10-23 23:25:52 +0700

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -44,7 +44,7 @@ import { UserAnswerLogModule } from './modules/user-answer-log/user-answer-log.m
 import { QuestionBankModule } from './modules/question-bank/question-bank.module';
 import { UserExerciseAttemptModule } from './modules/user-exercise-attempt/user-exercise-attempt.module';
 import { UserProgressModule } from './modules/user-progress/user-progress.module';
-import { AchievementModule } from './modules/achievement/achievement.module';
+
 
 @Module({
   imports: [
@@ -89,7 +89,7 @@ import { AchievementModule } from './modules/achievement/achievement.module';
     QuestionBankModule,
     UserExerciseAttemptModule,
     UserProgressModule,
-    AchievementModule
+    AchievementGroupModule
 
   ],
 
